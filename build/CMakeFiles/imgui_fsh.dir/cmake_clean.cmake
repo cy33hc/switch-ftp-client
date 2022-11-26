9e52f30dfd22a142b2fce060d0f1f93644c45560
Chee Yee <cheeyee09@gmail.com>
2022-11-26 01:29:20 -0800

    remove build files

--- build/CMakeFiles/imgui_fsh.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/imgui_fsh"
-  "imgui_fsh.dksh"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/imgui_fsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
